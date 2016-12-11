12fec0ec7af33bd472ae7394492efd4e50d00f6f
Valentin Lorentz <progval+git@progval.net>
2016-12-11 12:00:02 +0100

    Give config file name as optional command-line argument.

--- nxanimate/main.py
+++ nxanimate/main.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from .controller import Controller
 from .gui.main import start as gui_start
 from .debugger.patch_networkx import patch_networkx
@@ -5,7 +7,16 @@ from .debugger.patch_networkx import patch_networkx
 def main():
     patch_networkx()
     controller = Controller()
+
+    if len(sys.argv) == 1:
+        config = None
+    elif len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
+        config = sys.argv[1]
+    else:
+        print('Syntax: {} [config.ini]'.format(sys.argv[0]))
+        exit(1)
+
     gui_start(
             controller=controller,
-            config='config.ini',
+            config=config,
             )
